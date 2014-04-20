58e848810f3fcc6aa85da7b2f0ac9d7d550204d3
aktar chowdhury <aktarchowdhury12@gmail.com>
2014-04-20 00:56:28 -0400

    SeleNium FaceBook Registration

---
+++ SeleniumHomeWork1/src/com/aktar/sele1/HomeWork2.java
@@ -0,0 +1,12 @@
+package com.aktar.sele1;
+
+import org.junit.Test;
+
+public class HomeWork2 extends HomeWork1 {
+@Test
+public void HomePage(){
+	key.type("email", "aktarchowdhury7@gmail.com");
+	key.type("pass", "aa786786");
+	key.click("u_0_n");
+}
+}
